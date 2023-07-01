8819deffb55d7c8aca38668d53c3984971179484
AlexQ3D <metroid3d2@gmail.com>
2023-07-01 12:32:58 -0700

    Player control tweaks

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Movement
-    [SerializeField] private float moveSpeed = 12f; // top speed
-    [SerializeField] private float acceleration = 2f; // acceleration to top speed
-    [SerializeField] private float jumpSpeed = 3f;
+    [SerializeField] public float moveSpeed = 12f; // top speed
+    [SerializeField] public float acceleration = 2f; // acceleration to top speed
+    [SerializeField] public float jumpSpeed = 3f;
     private float horizontalMovement; // input for horizontal movement input
     private float horizontalSpeed, verticalSpeed; 
 
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
     private float jumpPressedTimer;
     private bool jumpPressed;
 
+    public int MaxJumps = 1;
+    private int availableJumps = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +31,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private bool CanJump() {
-        return (IsGrounded() || jumpBufferTimer > 0);
+        return ((IsGrounded() || jumpBufferTimer > 0) && availableJumps > 0);
     }
 
     private bool IsGrounded() {
@@ -42,27 +45,33 @@ public class PlayerController : MonoBehaviour
         horizontalMovement = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump")) {
-            jumpPressedTimer = 60;
+            jumpPressedTimer = 20;
         }
 
         jumpPressed = Input.GetButton("Jump");
-        
+       
     }
 
     private void FixedUpdate() {
         verticalSpeed = rb.velocity.y;
-
+        jumpPressedTimer--;
+        Debug.Log(jumpBufferTimer);
         if (!IsGrounded()) {
             jumpBufferTimer--;
         } else {
             jumpBufferTimer = jumpBufferTime;
+            availableJumps = MaxJumps;
         }
 
         // Redefine acceleration so we can modify it mid run
         float accel = acceleration;
         
         if (Mathf.Sign(horizontalMovement) != Mathf.Sign(horizontalSpeed)) {
-            accel *= 2;
+            if (IsGrounded()) {
+                accel *= 0.95f;
+            } else {
+                accel *= .75f;
+            }
         }
 
 
@@ -77,12 +86,13 @@ public class PlayerController : MonoBehaviour
 
         if (CanJump() && jumpPressedTimer > 0) {
             verticalSpeed = jumpSpeed;
+            availableJumps--;
             jumpPressedTimer = 0;
             jumpBufferTimer = 0;
         }
 
         if (!jumpPressed && verticalSpeed > 0) {
-            verticalSpeed *= 0.75f;
+            verticalSpeed *= 0.875f;
         }
 
         rb.velocity = new Vector2(horizontalSpeed, verticalSpeed);
